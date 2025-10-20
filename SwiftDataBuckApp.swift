5c7be885ca1e7cfc574d6768d2b107971b40d675
Augusto Orozco <a00839579@tec.mx>
2025-10-20 13:51:01 -0600

    Commit1

---
+++ SwiftDataBuckApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataBuckApp.swift
+//  SwiftDataBuck
+//
+//  Created by Alumno on 20/10/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataBuckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
